98de3cc600926704c474c39485fd76d37915aff2
Matthew Oro <MatthewEOro@gmail.com>
2021-11-25 15:36:51 -0500

    add styling to room component

--- client/src/pages/Dashboard/Rooms.tsx
+++ client/src/pages/Dashboard/Rooms.tsx
@@ -1,10 +1,13 @@
 import React, { useRef, useState } from "react";
+import "./Rooms.scss";
 import useSockets from "../../context/SocketContext";
 import EVENTS from "../../config/socketEvents";
 import Avatar from "@mui/material/Avatar";
 import TextField from "@mui/material/TextField";
 import { teal } from "@mui/material/colors";
 import Modal from "@mui/material/Modal";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
 
 const Rooms = ({ currentUser }: { currentUser: any }) => {
   const { socket, currentRoomId, rooms } = useSockets();
@@ -23,6 +26,7 @@ const Rooms = ({ currentUser }: { currentUser: any }) => {
     }
     socket.emit(EVENTS.CLIENT.CREATE_ROOM, { roomName, currentUser });
     setRoomName("");
+    setShowModal(false);
   };
   const handleJoinRoom = async (e: any) => {
     const clickedRoom = e.target.id;
@@ -47,12 +51,28 @@ const Rooms = ({ currentUser }: { currentUser: any }) => {
                 value={roomName}
                 onChange={handleTextFieldChange}
                 variant="standard"
+                style={{ margin: 0 }}
               />
-              <button onClick={handleCreateRoom}>Enter</button>
+              <Button
+                style={{ backgroundColor: teal["A400"] }}
+                variant="contained"
+                onClick={handleCreateRoom}
+              >
+                Confirm
+              </Button>
             </div>
           </Modal>
           <section>
-            <div className="room__wrapper room__add-btn">ADD ROOMS</div>
+            <div className="room__wrapper room__add-btn" onClick={handleOpen}>
+              <AddIcon
+                style={{
+                  color: teal["A400"],
+                  pointerEvents: "none",
+                  fontSize: "3rem",
+                }}
+                className="room__add-icon"
+              />
+            </div>
             {rooms.map((elem) => {
               return (
                 <div
@@ -61,7 +81,10 @@ const Rooms = ({ currentUser }: { currentUser: any }) => {
                   className="room__wrapper"
                   onClick={handleJoinRoom}
                 >
-                  <Avatar sx={{ bgcolor: teal["A400"] }}>
+                  <Avatar
+                    sx={{ bgcolor: teal["A400"] }}
+                    className="room__avatar"
+                  >
                     {elem.title[0]}
                   </Avatar>
                   <p className="room__title">{elem.title}</p>
