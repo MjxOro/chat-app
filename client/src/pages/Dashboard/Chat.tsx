c4d9c61b677a0552edbaa24b09fa41385843e1ec
Matthew Oro <MatthewEOro@gmail.com>
2021-11-28 12:57:56 -0500

    made sender messages on the right and recieving on the left

--- client/src/pages/Dashboard/Chat.tsx
+++ client/src/pages/Dashboard/Chat.tsx
@@ -5,7 +5,7 @@ import { ScrollControls, Scroll, Html, useScroll } from "@react-three/drei";
 import "./Chat.scss";
 import { useFrame, useThree } from "@react-three/fiber";
 
-const ChatMessages = ({ messages, setScrollHook }: any) => {
+const ChatMessages = ({ messages, setScrollHook, currentUser }: any) => {
   const data = useScroll();
   //pass hook value to parent component
   //ThreeJs renderer will update scroller height when messages composed
@@ -17,11 +17,18 @@ const ChatMessages = ({ messages, setScrollHook }: any) => {
     <>
       {messages.map((elem: any, index: any) => {
         const position = index * 10;
+        let isMe = "0";
+
+        if (elem._senderId !== currentUser._id) {
+          isMe = "-40vw";
+        } else {
+          isMe = "10vw";
+        }
         return (
           <div
             key={elem._id}
             className="chat__message"
-            style={{ position: "absolute", top: `${position}vh` }}
+            style={{ position: "absolute", top: `${position}vh`, left: isMe }}
           >
             <div>{elem.content}</div>
             <div>{elem.owner}</div>
@@ -41,14 +48,14 @@ const Chat = ({
   const [scrollHook, setScrollHook] = useState<any>(null);
   const setInitScrollY = async () => {
     if (scrollHook) {
-      scrollHook.offset = 0.99;
+      scrollHook.el.scrollTo(0, scrollHook.el.scrollHeight);
+      //scrollHook.offset = 1;
     }
   };
   //Always points to the Latest message: sending or recieving
   useEffect(() => {
-    if (scrollHook) {
-      setInitScrollY();
-    }
+    setInitScrollY();
+    console.log("state Change");
   }, [scrollHook]);
   const handleSendMessage = (e: any) => {
     e.preventDefault();
@@ -73,11 +80,15 @@ const Chat = ({
     <>
       <ScrollControls
         distance={1}
-        damping={5}
+        damping={100}
         pages={messages.length / 10 || 0}
       >
         <Scroll html>
-          <ChatMessages messages={messages} setScrollHook={setScrollHook} />
+          <ChatMessages
+            messages={messages}
+            setScrollHook={setScrollHook}
+            currentUser={currentUser}
+          />
         </Scroll>
       </ScrollControls>
       <Html position={[viewport.width * 0.01, -viewport.height * 0.4, 0]}>
