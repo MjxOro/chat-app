4a79b085afe6f5e0bf88f2ce4fa2c215ce2b4418
Matthew Oro <MatthewEOro@gmail.com>
2021-11-29 18:57:27 -0500

    fix routing for oaut

--- client/src/Pages/Landing/Landing.tsx
+++ client/src/Pages/Landing/Landing.tsx
@@ -37,9 +37,7 @@ const LandingPage: React.FC = () => {
                   src={googleOAuthBtn}
                   className="landing__oauth"
                   onClick={() => {
-                    window.location.href = `${
-                      process.env.REACT_APP_HOST as string
-                    }/api/oauth/google`;
+                    window.location.href = "/api/oauth/google";
                   }}
                 />
               </div>
