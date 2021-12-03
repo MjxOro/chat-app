871b9f7028f4bd40983bed3716209bdaa75bee6f
Matthew Oro <MatthewEOro@gmail.com>
2021-12-02 19:12:39 -0500

    add extra graphics to chat ui

--- client/src/Pages/Dashboard/Chat.tsx
+++ client/src/Pages/Dashboard/Chat.tsx
@@ -6,95 +6,90 @@ import {
   Html,
   useScroll,
   Text,
+  RoundedBox,
 } from "@react-three/drei";
 import "./Chat.scss";
 import { useFrame, useThree } from "@react-three/fiber";
 import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import { teal } from "@mui/material/colors";
 
 const ChatMessages = ({
   messages,
   setScrollHook,
   currentroomId,
   currentUser,
-  viewport,
 }: any) => {
   const data = useScroll();
-  const { camera } = useThree();
   const messageRef: any = useRef([]);
   //pass hook value to parent component
   //ThreeJs renderer will update scroller height when messages composed
+  const handleMouseMove = (e: any) => {
+    const cursorX = e.clientX / window.innerWidth - 1;
+    const cursorY = -((e.clientY * 0.25) / window.innerHeight - 2);
+  };
+  window.addEventListener("mousemove", handleMouseMove);
   useFrame(() => {
     setScrollHook(data);
+    if (messageRef.current.length) {
+      messages.forEach((elem: any, index: number) => {
+        messageRef.current[index].rotation.y += 0.01;
+      });
+    }
   });
   useEffect(() => {
     return window.removeEventListener("mousemove", handleMouseMove);
   }, [currentroomId]);
-  const handleMouseMove = (e: any) => {
-    const cursorX = e.clientX / window.innerWidth - 1;
-    const cursorY = -((e.clientY * 0.25) / window.innerHeight - 2);
-    if (messageRef.current[messages.length - 1]) {
-      messages.forEach((elem: any, index: number) => {
-        if (elem._senderId === currentUser._id) {
-          messageRef.current[index].position.y = -cursorY;
-          messageRef.current[index].rotation.y = cursorX / 5;
-        } else {
-          messageRef.current[index].position.y = -cursorY;
-          messageRef.current[index].rotation.y = -cursorX / 5;
-        }
-      });
-    }
-  };
-  window.addEventListener("mousemove", handleMouseMove);
 
   return (
     <>
-      {messages.map((elem: any, index: number) => {
-        const position = -index;
-        const isMe = (): number => {
-          if (elem._senderId === currentUser._id) {
-            return 5;
-          } else {
-            return -1.75;
-          }
-        };
+      <group>
+        {messages.map((elem: any, index: number) => {
+          const position = -index;
+          const isMe = (): number => {
+            if (elem._senderId === currentUser._id) {
+              return 5;
+            } else {
+              return -1.75;
+            }
+          };
 
-        return (
-          <group
-            ref={(el) => {
-              messageRef.current[index] = el;
-            }}
-            key={elem._id}
-            position={[isMe(), -2, 0]}
-          >
-            <Text
-              anchorX={elem._senderId === currentUser._id ? "right" : "left"}
-              color="black"
-              position={[0, position + 3.25, 0]}
-            >
-              {elem.owner}
-            </Text>
-            <Text
-              anchorX={elem._senderId === currentUser._id ? "right" : "left"}
-              color="black"
-              position={[0, position + 3, 0]}
-            >
-              {elem.content}
-            </Text>
-          </group>
-          /*
-          <div
-            key={elem._id}
-            className="chat__message"
-            style={{ position: "absolute", top: `${position}vh`, left: isMe }}
-          >
-            <div>{elem.content}</div>
-            <div>{elem.owner}</div>
-          </div>
-          */
-        );
-      })}
+          return (
+            <>
+              <RoundedBox
+                ref={(el) => {
+                  messageRef.current[index] = el;
+                }}
+                key={elem._id}
+                args={[0.25, 0.5, 0.1]}
+                radius={0.1}
+                smoothness={2}
+                position={[isMe() + 0.15, position + 3, -0.2]}
+              >
+                <meshToonMaterial
+                  color={
+                    elem._senderId === currentUser._id ? "#1de9b6" : "pink"
+                  }
+                  attach="material"
+                />
+              </RoundedBox>
+              <Text
+                anchorX={elem._senderId === currentUser._id ? "right" : "left"}
+                position={[isMe(), position + 3.25, 0]}
+              >
+                <meshToonMaterial color="black" attach="material" />
+                {elem.owner}
+              </Text>
+              <Text
+                anchorX={elem._senderId === currentUser._id ? "right" : "left"}
+                color="black"
+                position={[isMe(), position + 3, 0]}
+              >
+                <meshToonMaterial color="black" attach="material" />
+                {elem.content}
+              </Text>
+            </>
+          );
+        })}
+      </group>
     </>
   );
 };
@@ -141,6 +136,7 @@ const Chat = ({
   return (
     <>
       <ScrollControls
+        eps={1}
         distance={1}
         damping={100}
         pages={(messages.length + 3) / viewport.height || 0}
--- client/src/Pages/Dashboard/Dashboard.tsx
+++ client/src/Pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, Suspense, useState } from "react";
 import Chat from "./Chat";
 import useAuthenticate, {
   AuthContext,
@@ -6,7 +6,7 @@ import useAuthenticate, {
 } from "../../context/AuthContext";
 import Rooms from "./Rooms";
 import { Canvas } from "@react-three/fiber";
-import { Html } from "@react-three/drei";
+import { Html, Preload } from "@react-three/drei";
 import useSockets from "../../context/SocketContext";
 
 const Dashboard: React.FC = () => {
@@ -15,20 +15,25 @@ const Dashboard: React.FC = () => {
   return (
     <>
       <Canvas>
-        <Html style={{ transform: "translate(-166%,-50%)" }}>
-          <Rooms
+        <ambientLight />
+        <pointLight position={[-1, 0, 0]} />
+        <Suspense fallback={null}>
+          <Html style={{ transform: "translate(-166%,-50%)" }}>
+            <Rooms
+              isAuth={isAuth}
+              socket={socket}
+              rooms={rooms}
+              currentRoomId={currentRoomId}
+            />
+          </Html>
+          <Chat
             isAuth={isAuth}
             socket={socket}
-            rooms={rooms}
+            messages={messages}
             currentRoomId={currentRoomId}
           />
-        </Html>
-        <Chat
-          isAuth={isAuth}
-          socket={socket}
-          messages={messages}
-          currentRoomId={currentRoomId}
-        />
+          <Preload />
+        </Suspense>
       </Canvas>
     </>
   );
